d55e2294f37f5b6617df38876fdd44a974dcada8
inverse231 <3xinverse@gmail.com>
2019-11-08 01:10:30 +0300

    almost finished slider

--- slider/script.js
+++ slider/script.js
@@ -7,24 +7,43 @@ var i = 0;
 var fwdButton = document.createElement("button");
 var backButton = document.createElement("button");
 var img = document.createElement("img");
-img = imgArray[0];
 fwdButton.setAttribute("id", "f");
 backButton.setAttribute("id", "b");
 fwdButton.innerText = "forward";
 backButton.innerText = "back";
 var div = document.createElement("div");
+imgArray[0].classList.add("visible");
 div.appendChild(img);
 div.appendChild(backButton);
 div.appendChild(fwdButton);
 document.body.appendChild(div);
 
 
-fwdButton.addEventListener("click", changePhoto(this));
+
+
+fwdButton.addEventListener("click", function () {
+
+    if (i == imgArray.length) {
+        imgArray[i].classList.remove("visible");
+        i = 0;
+        imgArray[i].classList.add("visible");
+    } else {
+        imgArray[i].classList.remove("visible");
+        imgArray[++i].classList.add("visible");
+    }
+    console.log(i, imgArray.length);
+});
+
+backButton.addEventListener("click", function () {
+
+
+});
 
 function changePhoto(button) {
     if (button.id == "f") {
-        img.classList.add("visible");
+        imgArray[i++].classList.remove("visible");
+        imgArray[i - 1].classList.add("visible");
     } else {
-        
+
     }
 }
